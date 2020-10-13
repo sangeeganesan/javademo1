529af4460751b40d84273537140824038c74306b
unknown <sangeethatg7@gmail.com>
2020-10-13 14:26:20 +0530

    springmvcdemo

---
+++ Springdemo1/User1.java
@@ -0,0 +1,23 @@
+package com.model;
+
+public class User1 {
+	private String userName;
+	private String password;
+	public User1() {
+		super();
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+
+}

84b1b6c3363ff5a1b9ed8da19eaf0134b2c97d41
unknown <sangeethatg7@gmail.com>
2020-10-15 21:01:16 +0530

    Hibernatedemo

---
+++ Hibernatedemo1/java/com/main/MainApplication.java
@@ -0,0 +1,34 @@
+package com.main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import com.model.Employee;
+
+public class MainApplication {
+	public static void main(String[] args) {
+
+		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("com/config/hibernate.cfg.xml").build();
+		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+		SessionFactory factory =meta.getSessionFactoryBuilder().build(); 
+		Session session = factory.openSession(); 
+		Transaction transaction = session.beginTransaction(); 
+		Employee employee= new Employee();
+		employee.setEmpNo(10);;
+		employee.setEmpName("Hibernate demo 4");
+		employee.setSalary(666f);
+		session.save(employee); 
+		transaction.commit();
+		System.out.println("successfully saved");
+		
+		session.close(); 
+		factory.close();
+
+	}
+
+}
